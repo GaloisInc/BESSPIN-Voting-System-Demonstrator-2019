483f5e3639d4f8ef1c554e11082395c47b00d0e7
Joseph Kiniry <kiniry@freeandfair.us>
2019-06-07 16:04:57 -0700

    Revised Lando specs for SBB and began to write the ACSL spec.
    Note that comments on features are redundant, as they are clear from
    the refinement from the Lando model to code.
    
    Code was reformatted for my basic standard syntactic conventions.

--- source/sbb/sbb.c
+++ source/sbb/sbb.c
@@ -15,6 +15,7 @@
 // Subsystem includes
 #include "sbb.h"
 
+// BESSPIN Voting System devices
 #include "gpio.h"
 #include "serLcd.h"
 
@@ -26,7 +27,7 @@
 #define BALLOT_DETECT_TIMEOUT_MS 6000
 #define CAST_OR_SPOIL_TIMEOUT_MS 30000
 
-// Button defines
+// Buttons
 #define BUTTON_CAST_LED 1
 #define BUTTON_SPOIL_LED 3
 #define BUTTON_CAST_IN 0
@@ -43,237 +44,131 @@ bool barcode_present = false;
 char barcode[BARCODE_MAX_LENGTH] = {0};
 SemaphoreHandle_t barcode_mutex;
 
-/**
- * Initialize peripherals
- */
-void init_sbb(void)
-{
-    gpio_set_as_input(BUTTON_CAST_IN);
-    gpio_set_as_input(BUTTON_SPOIL_IN);
-    gpio_set_as_input(PAPER_SENSOR_IN);
-    gpio_set_as_input(PAPER_SENSOR_OUT);
-    gpio_set_as_output(MOTOR_0);
-    gpio_set_as_output(MOTOR_1);
-    gpio_set_as_output(BUTTON_CAST_LED);
-    gpio_set_as_output(BUTTON_SPOIL_LED);
-
-    barcode_mutex = xSemaphoreCreateMutex();
+void initialize(void) {
+  gpio_set_as_input(BUTTON_CAST_IN);
+  gpio_set_as_input(BUTTON_SPOIL_IN);
+  gpio_set_as_input(PAPER_SENSOR_IN);
+  gpio_set_as_input(PAPER_SENSOR_OUT);
+  gpio_set_as_output(MOTOR_0);
+  gpio_set_as_output(MOTOR_1);
+  gpio_set_as_output(BUTTON_CAST_LED);
+  gpio_set_as_output(BUTTON_SPOIL_LED);
+  
+  barcode_mutex = xSemaphoreCreateMutex();
 }
 
-/**
- * Perform Tally!
- */
-void perform_tally(void) { printf("Performing tally\r\n"); }
+void perform_tabulation(void) { printf("Performing tabulation!\r\n"); }
 
-/**
- * Is barcode valid?
- * Check validity of the given barcode string
- */
-bool is_barcode_valid(char *str, uint8_t len)
-{
-    (void)str;
-    (void)len;
-    return true;
+bool is_barcode_valid(barcode the_barcode, barcode_length its_length) {
+  (void)str;
+  (void)len;
+  return true;
 }
 
-/**
- * Is Cast button pressed?
- */
 bool is_cast_button_pressed(void) { return gpio_read(BUTTON_CAST_IN); }
 
-/**
- * Is Spoil button pressed?
- */
 bool is_spoil_button_pressed(void) { return gpio_read(BUTTON_SPOIL_IN); }
 
-/**
- * Just received barcode!
- */
-void just_received_barcode(void)
-{
-    if (xSemaphoreTake(barcode_mutex, portMAX_DELAY) == pdTRUE)
-    {
-        barcode_present = true;
-        xSemaphoreGive(barcode_mutex);
-    }
+void just_received_barcode(void) {
+  if (xSemaphoreTake(barcode_mutex, portMAX_DELAY) == pdTRUE) {
+    barcode_present = true;
+    xSemaphoreGive(barcode_mutex);
+  }
 }
 
-/**
- * Set barcode to this value
- */
-void set_received_barcode(char *str, uint8_t len)
-{
-    configASSERT(len <= BARCODE_MAX_LENGTH);
-    if (xSemaphoreTake(barcode_mutex, portMAX_DELAY) == pdTRUE)
-    {
-        memcpy(barcode, str, len);
-        xSemaphoreGive(barcode_mutex);
-    }
+void set_received_barcode(barcode the_barcode, barcode_length its_length) {
+  configASSERT(its_length <= BARCODE_MAX_LENGTH);
+  if (xSemaphoreTake(barcode_mutex, portMAX_DELAY) == pdTRUE) {
+    memcpy(barcode, the_barcode, its_length);
+    xSemaphoreGive(barcode_mutex);
+  }
 }
 
-/**
- * Has a barcode?
- */
-bool has_a_barcode(void)
-{
-    bool val = false;
-    if (xSemaphoreTake(barcode_mutex, portMAX_DELAY) == pdTRUE)
-    {
-        val = barcode_present;
-        xSemaphoreGive(barcode_mutex);
-    }
-    return val;
+bool has_a_barcode(void) {
+  bool val = false;
+  if (xSemaphoreTake(barcode_mutex, portMAX_DELAY) == pdTRUE) {
+    val = barcode_present;
+    xSemaphoreGive(barcode_mutex);
+  }
+  return val;
 }
 
-/**
- * What is the barcode?
- */
-void what_is_the_barcode(char *str, uint8_t len)
-{
-    configASSERT(len <= BARCODE_MAX_LENGTH);
-    if (xSemaphoreTake(barcode_mutex, portMAX_DELAY) == pdTRUE)
-    {
-        memcpy(str, barcode, len);
-        xSemaphoreGive(barcode_mutex);
-    }
+void what_is_the_barcode(barcode the_barcode, barcode_length its_length) {
+  configASSERT(its_length <= BARCODE_MAX_LENGTH);
+  if (xSemaphoreTake(barcode_mutex, portMAX_DELAY) == pdTRUE) {
+    memcpy(the_barcode, barcode, its_length);
+    xSemaphoreGive(barcode_mutex);
+  }
 }
 
-/**
- * Spoil Button Light On!
- */
 void spoil_button_light_on(void) { gpio_write(BUTTON_SPOIL_LED); }
 
-/**
- * Spoil Button Light Off!
- */
 void spoil_button_light_off(void) { gpio_clear(BUTTON_SPOIL_LED); }
 
-/**
- * Cast Button Light On!
- */
 void cast_button_light_on(void) { gpio_write(BUTTON_CAST_LED); }
 
-/**
- * Cast Button Light Off!
- */
 void cast_button_light_off(void) { gpio_clear(BUTTON_CAST_LED); }
 
-/**
- * Move Motor Forward!
- */
-void move_motor_forward(void)
-{
-    gpio_write(MOTOR_0);
-    gpio_clear(MOTOR_1);
+void move_motor_forward(void) {
+  gpio_write(MOTOR_0);
+  gpio_clear(MOTOR_1);
 }
 
-/**
- * Move Motor back!
- */
-void move_motor_back(void)
-{
-    gpio_clear(MOTOR_0);
-    gpio_write(MOTOR_1);
+void move_motor_back(void) {
+  gpio_clear(MOTOR_0);
+  gpio_write(MOTOR_1);
 }
 
-/**
- * Stop Motor!
- */
-void stop_motor(void)
-{
-    gpio_clear(MOTOR_0);
-    gpio_clear(MOTOR_1);
+void stop_motor(void) {
+  gpio_clear(MOTOR_0);
+  gpio_clear(MOTOR_1);
 }
 
-/**
- * Display this text!
- */
 void display_this_text(char *str, uint8_t len) { serLcdPrintf(str, len); }
 
-/**
- * Ballot detected?
- */
 bool ballot_detected(void) { return gpio_read(PAPER_SENSOR_IN); }
 
-/**
- * Ballot inserted?
- */
 bool ballot_inserted(void) { return gpio_read(PAPER_SENSOR_OUT); }
 
-/**
- * Spoil ballot!
- */
-void spoil_ballot(void)
-{
-    move_motor_back();
-    while (!(ballot_detected() && !ballot_inserted()))
-    {
-        ;
-        ;
-    }
-    stop_motor();
+void spoil_ballot(void) {
+  move_motor_back();
+  while (!(ballot_detected() && !ballot_inserted())) {
+    ;
+  }
+  stop_motor();
 }
 
-/**
- * Cast ballot!
- */
-void cast_ballot(void)
-{
-    move_motor_forward();
-    while (!(!ballot_detected() && ballot_inserted()))
-    {
-        ;
-        ;
-    }
-    stop_motor();
+void cast_ballot(void) {
+  move_motor_forward();
+  while (!(!ballot_detected() && ballot_inserted())) {
+    ;
+  }
+  stop_motor();
 }
 
-/**
- * Ballot spoiled?
- */
 bool ballot_spoiled(void) { return (!ballot_detected() && !ballot_inserted()); }
 
-/**
- * Go to standby!
- */
-void go_to_standby(void)
-{
-    stop_motor();
-    cast_button_light_off();
-    spoil_button_light_off();
-    display_this_text(insert_ballot_text, strlen(insert_ballot_text));
+void go_to_standby(void) {
+  stop_motor();
+  cast_button_light_off();
+  spoil_button_light_off();
+  display_this_text(insert_ballot_text, strlen(insert_ballot_text));
 }
 
-/**
- * Ballot Detect Timeout Reset!
- */
-void ballot_detect_timeout_reset(void)
-{
-    ballot_detect_timeout =
-        xTaskGetTickCount() + pdMS_TO_TICKS(BALLOT_DETECT_TIMEOUT_MS);
+void ballot_detect_timeout_reset(void) {
+  ballot_detect_timeout =
+    xTaskGetTickCount() + pdMS_TO_TICKS(BALLOT_DETECT_TIMEOUT_MS);
 }
 
-/**
- * Ballot Detect Timeout Expired?
- */
-bool ballot_detect_timeout_expired(void)
-{
-    return (xTaskGetTickCount() > ballot_detect_timeout);
+bool ballot_detect_timeout_expired(void) {
+  return (xTaskGetTickCount() > ballot_detect_timeout);
 }
 
-/**
- * Cast Or Spoil Timeout Reset!
- */
-void cast_or_spoil_timeout_reset(void)
-{
-    cast_or_spoil_timeout =
-        xTaskGetTickCount() + pdMS_TO_TICKS(CAST_OR_SPOIL_TIMEOUT_MS);
+void cast_or_spoil_timeout_reset(void) {
+  cast_or_spoil_timeout =
+    xTaskGetTickCount() + pdMS_TO_TICKS(CAST_OR_SPOIL_TIMEOUT_MS);
 }
 
-/**
- * Cast Or Spoil Timeout Expired?
- */
-bool cast_or_spoil_timeout_expired(void)
-{
-    return (xTaskGetTickCount() > cast_or_spoil_timeout);
+bool cast_or_spoil_timeout_expired(void) {
+  return (xTaskGetTickCount() > cast_or_spoil_timeout);
 }
--- source/sbb/sbb.h
+++ source/sbb/sbb.h
@@ -25,137 +25,58 @@ extern char *remove_ballot_text;
 /**
  * Initialize peripherals
  */
-void init_sbb(void);
+void initialize(void);
 
-/**
- * Perform Tally!
- */
-void perform_tally(void);
+void perform_tabulation(void);
 
-/**
- * Is barcode valid?
- * Check validity of the given barcode string
- */
 bool is_barcode_valid(barcode the_barcode, barcode_length its_length);
 
-/**
- * Is Cast button pressed?
- */
 bool is_cast_button_pressed(void);
 
-/**
- * Is Spoil button pressed?
- */
 bool is_spoil_button_pressed(void);
 
-/**
- * Has a barcode?
- */
 bool has_a_barcode(void);
 
-/**
- * Just received barcode!
- */
 void just_received_barcode(void);
 
-/**
- * Set barcode to this value
- */
 void set_received_barcode(barcode the_barcode, barcode_length its_length);
 
-/**
- * What is the barcode?
- */
 void what_is_the_barcode(barcode the_barcode, barcode_length its_length);
 
-/**
- * Spoil Button Light On!
- */
 void spoil_button_light_on(void);
 
-/**
- * Spoil Button Light Off!
- */
 void spoil_button_light_off(void);
 
-/**
- * Cast Button Light On!
- */
 void cast_button_light_on(void);
 
-/**
- * Cast Button Light Off!
- */
 void cast_button_light_off(void);
 
-/**
- * Move Motor Forward!
- */
 void move_motor_forward(void);
 
-/**
- * Move Motor back!
- */
 void move_motor_back(void);
 
-/**
- * Stop Motor!
- */
 void stop_motor(void);
 
-/**
- * Display this text!
- */
 void display_this_text(char *the_string_to_display, uint8_t its_length);
 
-/**
- * Ballot detected?
- */
 bool ballot_detected(void);
 
-/**
- * Ballot inserted?
- */
 bool ballot_inserted(void);
 
-/**
- * Spoil ballot!
- */
 void spoil_ballot(void);
 
-/**
- * Cast ballot!
- */
 void cast_ballot(void);
 
-/**
- * Ballot spoiled?
- */
 bool ballot_spoiled(void);
 
-/**
- * Go to standby!
- */
 void go_to_standby(void);
 
-/**
- * Ballot Detect Timeout Reset!
- */
 void ballot_detect_timeout_reset(void);
 
-/**
- * Ballot Detect Timeout Expired?
- */
 bool ballot_detect_timeout_expired(void);
 
-/**
- * Cast Or Spoil Timeout Reset!
- */
 void cast_or_spoil_timeout_reset(void);
 
-/**
- * Cast Or Spoil Timeout Expired?
- */
 bool cast_or_spoil_timeout_expired(void);
 
 /**
