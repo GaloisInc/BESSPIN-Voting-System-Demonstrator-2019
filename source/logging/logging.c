153ebb86aae097d35593435daf3998666db31ef3
draganst <draganst@ie.ibm.com>
2019-06-08 01:01:09 +0100

    added logging API and acsl sceletons.

---
+++ source/logging/logging.c
@@ -0,0 +1,12 @@
+/**
+ * Smart Ballot Box API
+ * @refine sbb_security.lando
+ */
+
+// General includes
+#include <stdio.h>
+#include <string.h>
+
+// Subsystem includes
+#include "logging.h"
+
---
+++ source/logging/logging_t.h
@@ -0,0 +1,13 @@
+/**
+ * Smart Ballot Logging Types
+ * @refine sbb_security.lando
+ */
+
+#ifndef __LOGGING_T__
+#define __LOGGING_T__
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#endif
