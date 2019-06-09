9f8cc0e112d3463bd9e2a9b07166dcbb0369023e
draganst <draganst@ie.ibm.com>
2019-06-09 13:41:34 +0100

    updated Makefile.verification

--- source/logging/logging_t.h
+++ source/logging/logging_t.h
@@ -8,8 +8,7 @@
 
 #include <stddef.h>
 #include <stdbool.h>
-#include <stdint.h>
-
+#include "ff.h" /* Declarations of FatFs API */
 #define LOG_ENTRY_LENGTH 256
 
 typedef uint8_t log_entry[LOG_ENTRY_LENGTH];
