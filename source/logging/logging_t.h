d505a53170863c5a72e5f79dc052001c065f8eee
draganst <draganst@ie.ibm.com>
2019-06-09 17:56:51 +0100

    commented-out section for main method; uncommented incl of ff.h

--- source/logging/logging_t.h
+++ source/logging/logging_t.h
@@ -8,7 +8,9 @@
 
 #include <stddef.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include "ff.h" /* Declarations of FatFs API */
+
 #define LOG_ENTRY_LENGTH 256
 
 typedef uint8_t log_entry[LOG_ENTRY_LENGTH];
