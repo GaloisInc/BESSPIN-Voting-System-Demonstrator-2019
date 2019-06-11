82b78b324528af64fcf991da2636b3e3a6e83e0a
Alexander Bakst <abakst@galois.com>
2019-06-11 13:54:10 -0700

    add messages to state machine

--- source/sbb/sbb_machine.c
+++ source/sbb/sbb_machine.c
@@ -9,20 +9,40 @@
 
 // Subsystem includes
 #include "sbb.h"
-#include "sbb_t.h"
 #include "sbb.acsl"
+
+// Text defines
+char *insert_ballot_text = "Insert ballot.";
+char *barcode_detected_text = "Barcode detected.";
+char *cast_or_spoil_text = "Cast or Spoil?";
+char *casting_ballot_text = "Casting ballot.";
+char *spoiling_ballot_text = "Spoiling ballot.";
+char *not_a_valid_barcode_text = "Not a valid barcode!";
+char *no_barcode_text = "No barcode detected!";
+char *remove_ballot_text = "Remove ballot!";
+
+// @design kiniry Here is the explicit encoding of the SBB state.
+SBB_state the_state = { .S = START };
+
+// @todo kiniry This is a placeholder state representation so that we
+// can talk about the state of memory-mapped firmware.
+firmware_state the_firmware_state;
 
 // This main loop for the SBB never terminates until the system is
 // turned off.
 /*@ terminates \false;
   @ ensures    \false;
 */
-void ballot_box_machine_loop(void) {
+void ballot_box_main_loop(void) {
   char this_barcode[BARCODE_MAX_LENGTH] = {0};
   for(;;) {
     // Set initial states
-    go_to_standby();
     switch ( the_state.L ) {
+
+    case STANDBY:
+      go_to_standby();
+      the_state.L = WAIT_FOR_BALLOT;
+      break;
 
     case WAIT_FOR_BALLOT:
       if ( ballot_detected() ) {
@@ -40,20 +60,28 @@ void ballot_box_machine_loop(void) {
         if ( ballot_inserted() && has_a_barcode() ) {
           the_state.L = BARCODE_DETECTED;
         } else {
-          the_state.L = WAIT_FOR_BALLOT;
+          display_this_text(no_barcode_text, strlen(no_barcode_text));
+          the_state.L = SPOIL;
         }
       }
       break;
 
+    // Assume: has_a_barcode() == true. Is this in sbb.h?
     case BARCODE_DETECTED:
-      // Assume: has_a_barcode() == true. Is this in sbb.h?
+      display_this_text(barcode_detected_text,
+                        strlen(barcode_detected_text));
       what_is_the_barcode(this_barcode, BARCODE_MAX_LENGTH);
       if ( is_barcode_valid(this_barcode, BARCODE_MAX_LENGTH) ) {
         cast_button_light_on();
         spoil_button_light_on();
         cast_or_spoil_timeout_reset();
+
+        display_this_text(cast_or_spoil_text,
+                          strlen(cast_or_spoil_text));
         the_state.L = BARCODE_VALIDATED;
       } else {
+        display_this_text(not_a_valid_barcode_text,
+                          strlen(not_a_valid_barcode_text));
         the_state.L = SPOIL;
       }
       break;
@@ -64,7 +92,13 @@ void ballot_box_machine_loop(void) {
            || is_spoil_button_pressed() ) {
         spoil_button_light_off();
         cast_button_light_off();
-        the_state.L = is_cast_button_pressed() ? CAST : SPOIL;
+        if ( is_cast_button_pressed() ) {
+          display_this_text(casting_ballot_text,
+                            strlen(casting_ballot_text));
+          the_state.L = CAST;
+        } else {
+          the_state.L = SPOIL;
+        }
       }
       break;
 
@@ -74,7 +108,10 @@ void ballot_box_machine_loop(void) {
       break;
 
     case SPOIL:
+      display_this_text(spoiling_ballot_text,
+                        strlen(spoiling_ballot_text));
       spoil_ballot();
+      display_this_text(remove_ballot_text, strlen(remove_ballot_text));
       the_state.L = WAIT_FOR_SPOIL;
       break;
 
