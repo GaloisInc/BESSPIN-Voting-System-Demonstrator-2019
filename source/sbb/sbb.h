a6405cdc42c99a736efe37c63c45d9a728d5020a
Joseph Kiniry <kiniry@freeandfair.us>
2019-06-11 00:20:17 -0700

    Tighted frame axioms on SBB API & started to refined axiomatic defn.

--- source/sbb/sbb.h
+++ source/sbb/sbb.h
@@ -90,44 +90,37 @@ bool has_a_barcode(void);
 void what_is_the_barcode(barcode the_barcode, barcode_length its_length);
 
 // @review kiniry We have no ASM for button light states.
-/*@ assigns the_state.C, the_state.T, the_state.M, the_state.D,
-  @         the_state.P, the_state.B, the_state.BS, the_state.S;
+/*@ assigns the_state.button_illumination;
   @ ensures spoil_button_lit(the_state);
 */
 void spoil_button_light_on(void);
 
-/*@ assigns the_state.C, the_state.T, the_state.M, the_state.D,
-  @         the_state.P, the_state.B, the_state.BS, the_state.S;
+/*@ assigns the_state.button_illumination;
   @ ensures !spoil_button_lit(the_state);
 */
 void spoil_button_light_off(void);
 
-/*@ assigns the_state.C, the_state.T, the_state.M, the_state.D,
-  @         the_state.P, the_state.B, the_state.BS, the_state.S;
+/*@ assigns the_state.button_illumination;
   @ ensures cast_button_lit(the_state);
 */
 void cast_button_light_on(void);
 
-/*@ assigns the_state.C, the_state.T, the_state.M, the_state.D,
-  @         the_state.P, the_state.B, the_state.BS, the_state.S;
+/*@ assigns the_state.button_illumination;
   @ ensures !cast_button_lit(the_state);
 */
 void cast_button_light_off(void);
 
-/*@ assigns the_state.C, the_state.T, the_state.M, the_state.D,
-  @         the_state.P, the_state.B, the_state.BS, the_state.S;
+/*@ assigns the_state.M;
   @ ensures the_state.M == MOTORS_TURNING_FORWARD;
 */
 void move_motor_forward(void);
 
-/*@ assigns the_state.C, the_state.T, the_state.M, the_state.D,
-  @         the_state.P, the_state.B, the_state.BS, the_state.S;
+/*@ assigns the_state.M;
   @ ensures the_state.M == MOTORS_TURNING_BACKWARD;
 */
 void move_motor_back(void);
 
-/*@ assigns the_state.C, the_state.T, the_state.M, the_state.D,
-  @         the_state.P, the_state.B, the_state.BS, the_state.S;
+/*@ assigns the_state.M;
   @ ensures the_state.M == MOTORS_OFF;
 */
 void stop_motor(void);
@@ -136,7 +129,7 @@ void stop_motor(void);
 // be able to specify, both at the model and code level, what is on
 // the display.
 /*@ requires \valid_read(the_string_to_display + (0..its_length));
-  @ assigns \nothing;
+  @ assigns the_state.D;
   @ ensures the_state.D == SHOWING_TEXT;
 */
 void display_this_text(char* the_string_to_display, uint8_t its_length);
@@ -153,8 +146,8 @@ bool ballot_inserted(void);
 
 /*@ requires spoil_button_lit(the_state);
   @ requires the_state.P == EARLY_AND_LATE_DETECTED;
-  @ assigns the_state.C, the_state.T, the_state.M, the_state.D,
-  @         the_state.P, the_state.B, the_state.BS, the_state.S;
+  @ assigns the_state.button_illumination,
+  @         the_state.P;
   @ ensures no_buttons_lit(the_state);
   @ ensures the_state.P == NO_PAPER_DETECTED;
 */
@@ -162,26 +155,26 @@ void spoil_ballot(void);
 
 /*@ requires cast_button_lit(the_state);
   @ requires the_state.P == EARLY_AND_LATE_DETECTED;
-  @ assigns the_state.C, the_state.T, the_state.M, the_state.D,
-  @         the_state.P, the_state.B, the_state.BS, the_state.S;
+  @ assigns the_state.button_illumination,
+  @         the_state.P;
   @ ensures no_buttons_lit(the_state);
   @ ensures the_state.P == NO_PAPER_DETECTED;
 */
 void cast_ballot(void);
 
-/*@ assigns \nothing;
+/*@ assigns the_state.P;
   @ ensures \result == the_state.P == EARLY_PAPER_DETECTED;
 */
 bool ballot_spoiled(void);
 
 // Semi-equivalent to initialize() without firmware initialization.
 // @review Shouldn't calling this function clear the paper path?
-/*@ assigns the_state.C, the_state.T, the_state.M, the_state.D,
-  @         the_state.P, the_state.B, the_state.BS, the_state.S;
+/*@ assigns the_state.M, the_state.D, the_state.P, the_state.BS, the_state.S;
   @ ensures the_state.M == MOTORS_OFF;
   @ ensures the_state.D == SHOWING_TEXT;
   @ ensures the_state.P == NO_PAPER_DETECTED;
   @ ensures the_state.BS == BARCODE_NOT_PRESENT;
+  @ ensures the_state.S == INNER;
   @ ensures no_buttons_lit(the_state);
 */
 // @todo kiniry `insert_ballot_text` should be displayed.
