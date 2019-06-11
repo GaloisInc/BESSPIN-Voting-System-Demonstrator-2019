1fa7ec1fe701574ba530358d20ba059e590b04a0
Alexander Bakst <abakst@galois.com>
2019-06-11 14:00:50 -0700

    rename sbb_machine.c -> sbb_main.c

--- source/sbb/sbb_main.c
+++ source/sbb/sbb_main.c
@@ -1,5 +1,5 @@
 /**
- * Smart Ballot Box main
+ * Smart Ballot Box State Machine
  * @refine sbb.lando
  */
 
@@ -30,68 +30,100 @@ firmware_state the_firmware_state;
 
 // This main loop for the SBB never terminates until the system is
 // turned off.
-// @assurance kiniry Note that, for a non-trivial terminates clause,
-// we must prove that the function actually does terminate.
+/*@ terminates \false;
+  @ ensures    \false;
+*/
 void ballot_box_main_loop(void) {
   char this_barcode[BARCODE_MAX_LENGTH] = {0};
-  // Init happens before calling main()
-  for (;;) {
-    go_to_standby();
-    if (ballot_detected()) {
-      ballot_detect_timeout_reset();
-      while (!ballot_inserted() && !ballot_detect_timeout_expired()) {
+  for(;;) {
+    // Set initial states
+    switch ( the_state.L ) {
+
+    case STANDBY:
+      go_to_standby();
+      the_state.L = WAIT_FOR_BALLOT;
+      break;
+
+    case WAIT_FOR_BALLOT:
+      if ( ballot_detected() ) {
+        ballot_detect_timeout_reset();
         move_motor_forward();
+        the_state.L = FEED_BALLOT;
       }
-      stop_motor();
+      break;
 
-      if (has_a_barcode()) {
-        display_this_text(barcode_detected_text,
-                          strlen(barcode_detected_text));
-        what_is_the_barcode(this_barcode, BARCODE_MAX_LENGTH);
-
-        if (is_barcode_valid(this_barcode, BARCODE_MAX_LENGTH)) {
-          display_this_text(cast_or_spoil_text,
-                            strlen(cast_or_spoil_text));
-          cast_button_light_on();
-          spoil_button_light_on();
-          cast_or_spoil_timeout_reset();
-
-          while (!cast_or_spoil_timeout_expired() &&
-                 !is_cast_button_pressed() &&
-                 !is_spoil_button_pressed()) {
-            ;
-          }
-          if (is_cast_button_pressed()) {
-            spoil_button_light_off();
-            display_this_text(casting_ballot_text,
-                              strlen(casting_ballot_text));
-            cast_ballot();
-            cast_button_light_off();
-            continue; // directly to Standby
-          }
-          else {
-            cast_button_light_off();
-            spoil_button_light_off();
-            // ->  spoil ballot
-          }
-        }
-        else {
-          display_this_text(not_a_valid_barcode_text,
-                            strlen(not_a_valid_barcode_text));
-          // ->  spoil ballot
+    case FEED_BALLOT:
+      // Let the motor run. The next guard is the transition out of
+      // this state: either we have a ballot with a barcode
+      if ( ballot_inserted() || ballot_detect_timeout_expired() ) {
+        stop_motor();
+        if ( ballot_inserted() && has_a_barcode() ) {
+          the_state.L = BARCODE_DETECTED;
+        } else {
+          display_this_text(no_barcode_text, strlen(no_barcode_text));
+          the_state.L = SPOIL;
         }
       }
-      else { // if has_a_barcode
-        display_this_text(no_barcode_text, strlen(no_barcode_text));
-        // -> spoil ballot
+      break;
+
+    // Assume: has_a_barcode() == true. Is this in sbb.h?
+    case BARCODE_DETECTED:
+      display_this_text(barcode_detected_text,
+                        strlen(barcode_detected_text));
+      what_is_the_barcode(this_barcode, BARCODE_MAX_LENGTH);
+      if ( is_barcode_valid(this_barcode, BARCODE_MAX_LENGTH) ) {
+        cast_button_light_on();
+        spoil_button_light_on();
+        cast_or_spoil_timeout_reset();
+
+        display_this_text(cast_or_spoil_text,
+                          strlen(cast_or_spoil_text));
+        the_state.L = BARCODE_VALIDATED;
+      } else {
+        display_this_text(not_a_valid_barcode_text,
+                          strlen(not_a_valid_barcode_text));
+        the_state.L = SPOIL;
       }
+      break;
+
+    case BARCODE_VALIDATED:
+      if (    cast_or_spoil_timeout_expired()
+           || is_cast_button_pressed()
+           || is_spoil_button_pressed() ) {
+        spoil_button_light_off();
+        cast_button_light_off();
+        if ( is_cast_button_pressed() ) {
+          display_this_text(casting_ballot_text,
+                            strlen(casting_ballot_text));
+          the_state.L = CAST;
+        } else {
+          the_state.L = SPOIL;
+        }
+      }
+      break;
+
+    case CAST:
+      cast_ballot();
+      the_state.L = WAIT_FOR_BALLOT;
+      break;
+
+    case SPOIL:
       display_this_text(spoiling_ballot_text,
                         strlen(spoiling_ballot_text));
       spoil_ballot();
       display_this_text(remove_ballot_text, strlen(remove_ballot_text));
-      while (!ballot_spoiled()) {
-        ;
+      the_state.L = WAIT_FOR_SPOIL;
+      break;
+
+    case WAIT_FOR_SPOIL:
+      if ( ballot_spoiled() ) {
+        the_state.L = WAIT_FOR_BALLOT;
       }
-    } // if ballot_detected
-  }     // loop iteration
+      break;
+
+      //default:
+      //assert(false);
+    }
+  }
 }
+
