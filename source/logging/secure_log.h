e31f0da4f3fd889b196093d79ad8be3322c6a3af
draganst <draganst@ie.ibm.com>
2019-06-12 21:33:10 +0100

    added preconditions in secure_log.h

--- source/logging/secure_log.h
+++ source/logging/secure_log.h
@@ -25,9 +25,17 @@ secure_log create_secure_log(const secure_log_name the_secure_log_name,
 
 secure_log_entry secure_log_entry_kind(const secure_log_name a_secure_log_name);
 
+/*@ requires \valid_read(a_log_entry + (0 .. LOG_ENTRY_LENGTH -1));
+  @ requires \valid(the_secure_log);
+  @ requires \separated(the_secure_log, a_log_entry);
+@*/
 void write_entry_to_secure_log(const secure_log the_secure_log,
                                const log_entry a_log_entry);
 
+/*@
+  @requires \valid_read(the_secure_log_entry.the_entry  + (0 .. LOG_ENTRY_LENGTH -1));
+  @requires \valid_read(the_secure_log_entry.the_digest + (0 .. SHA256_DIGEST_LENGTH_BYTES -1));
+@*/
 bool verify_secure_log_entry_security(const secure_log_entry the_secure_log_entry);
 
 bool verify_secure_log_security(const secure_log the_secure_log);
