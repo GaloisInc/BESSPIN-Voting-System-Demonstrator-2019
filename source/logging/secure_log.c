53a5a9c9bbb6fcd1558df7fa13df61f3b759124d
Rod Chapman <rod@proteancode.com>
2019-06-12 14:02:06 +0100

    Add dummy HSM API for now, and initial implementaion of Cryptol initialLogEntry

--- source/logging/secure_log.c
+++ source/logging/secure_log.c
@@ -7,6 +7,75 @@
 
 // Subsystem includes
 #include "secure_log.h"
+#include "hsm.h"
+
+// Local functions
+
+// Refines Cryptol initialLogEntry
+/*@
+    requires \valid_read (key + (0 .. AES256_KEY_LENGTH_BYTES - 1));
+    requires \valid_read (msg + (0 .. LOG_ENTRY_LENGTH - 1));
+    requires \separated (key, msg);
+    assigns \nothing;
+*/
+static secure_log_entry initial_log_entry(const aes256_key key, // IN
+			                  const log_entry msg)  // IN
+{
+  secure_log_entry initial_entry;
+
+  // 1. Zero-pad msg to exactly 256 chars with zeros.
+  // log_entry is exactly 256 chars, so we'll assume this has already been
+  // done by the caller.
+
+  // 2. Form "hmac key msg"
+  hmac (key, msg, LOG_ENTRY_LENGTH, &initial_entry.the_digest);
+
+  // 3. Copy the msg data
+  /*@
+      loop invariant 0 <= i <= LOG_ENTRY_LENGTH;
+      loop invariant \forall size_t j; 0 <= j < i ==> initial_entry.the_entry[j] == msg[j];
+      loop assigns i, initial_entry.the_entry[0 .. LOG_ENTRY_LENGTH - 1];
+      loop variant LOG_ENTRY_LENGTH - i;
+  */
+  for (size_t i = 0; i < LOG_ENTRY_LENGTH; i++)
+    {
+      initial_entry.the_entry[i] = msg[i];
+    }
+
+  return initial_entry; // struct by copy return
+}
+
+
+// Exported functions
+
+secure_log create_secure_log(const secure_log_name the_secure_log_name,
+                             const log_entry a_log_entry_type,
+                             const secure_log_security_policy the_policy)
+{
+
+  // Initial/Draft pseudo-code by RCC
+
+  // 1. Create new file, open for writing only.
+  // 2. call initial_log_entry above to create the first block
+  // 3. Write that new block to the file.
+  // 4. sync the file.
+  // 5. return the file handle as result
+
+  // TBDs - what about error cases?
+  //   What if the file already exists? Perhaps a pre-condition here that it doesn't
+  //    already exist, so up to the caller to spot that and do the right thing...
+  //    We may have to implement an f_exists() API (and ACSL logic function) to support
+  //    that if not directly supported by ff.h
+  //   What if the file create fails?
+  //   What is the file write fails?
+  //
+  // RCC I don't acually like the idea of leaving the file handle open all the time.
+  //     Why not close it at the end of each operation? Does this grant more integrity on failure?
+  
+}
+
+
+
 
 void write_entry_to_secure_log(const secure_log the_secure_log,
                                const log_entry a_log_entry) {
--- source/logging/secure_log_t.h
+++ source/logging/secure_log_t.h
@@ -2,17 +2,24 @@
 #define __SECURE_LOG_T__
 
 #include "log_t.h"
+#include "hsm.h"
 
 #define SECURE_LOG_ENTRY_LENGTH 256+sizeof(digest)
 
 typedef log_name secure_log_name;
 typedef log secure_log;
 typedef log_io_stream secure_log_io_stream;
-typedef uint8_t digest[256];
+
+
+// RCC thinks this is wrong - should be 32 bytes for 256 _bit_ digest.
+// And should be picked up from hsm.h anyway.
+// typedef uint8_t digest[256];
+
 typedef struct secure_log_entries {
-  log_entry the_entry;
-  digest the_digest;
+  log_entry     the_entry;
+  sha256_digest the_digest;
 } secure_log_entry;
+
 typedef enum { no_integrity, hashchain_sha2_256, hashchain_sha3_256 } integrity;
 typedef enum { no_provenance } provenance;
 typedef enum { no_confidentiality, all_plaintext, all_ciphertext } confidentiality;
