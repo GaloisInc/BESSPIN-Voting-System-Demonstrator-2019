16c645e09a75abd9ef79b119b523031d0bcf54a8
Daniel M. Zimmerman <dmz@galois.com>
2019-06-13 13:22:18 -0700

    Moved constant definitions into `crypto_defs.c`.

---
+++ source/crypto/crypto_defs.c
@@ -0,0 +1,14 @@
+/**
+ * SBB Crypto subysystem
+ * @refine crypto.lando
+ */
+
+// General includes
+
+// Subsystem includes
+#include "crypto.h"
+
+// constant definitions for the crypto subsystem
+const uint32_t AES_BLOCK_SIZE  = 256;
+const uint32_t AES_KEY_SIZE    = 256;
+const uint32_t SHA_DIGEST_SIZE = 256;
