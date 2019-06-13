0d6a6964c9edab582da3c23972a2b53117390911
Daniel M. Zimmerman <dmz@galois.com>
2019-06-13 13:32:02 -0700

    Fixed incorrect AES block size in constant declarations.

--- source/crypto/crypto_defs.c
+++ source/crypto/crypto_defs.c
@@ -9,6 +9,7 @@
 #include "crypto.h"
 
 // constant definitions for the crypto subsystem
-const uint32_t AES_BLOCK_SIZE  = 256;
+// Note: AES only supports 128-bit blocks regardless of key size
+const uint32_t AES_BLOCK_SIZE  = 128; 
 const uint32_t AES_KEY_SIZE    = 256;
 const uint32_t SHA_DIGEST_SIZE = 256;
