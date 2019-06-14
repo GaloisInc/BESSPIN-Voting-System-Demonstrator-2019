92c5481a4e9dcf9162bf434360c8eb0c98e20084
Rod Chapman <rod@proteancode.com>
2019-06-14 14:50:58 +0100

    First implementaion of Log validation functions

--- source/logging/secure_log.c
+++ source/logging/secure_log.c
@@ -13,7 +13,7 @@
 
 const aes256_key test_key = { 0 };
 
-//keep  sha256_digest
+// Local persistent state
 
 static sha256_digest previous_hash;
 
@@ -126,7 +126,6 @@ void write_entry_to_secure_log(const secure_log the_secure_log,
     }
 
    // invoke hash ( paddedMsg # previousHash)
-   //   sha256 (msg,SECURE_LOG_ENTRY_LENGTH, &current_entry.the_digest[0]);
    sha256 (msg,SECURE_LOG_ENTRY_LENGTH, &new_hash[0]);
    
    // Add the a_log_entry to the current_entry
@@ -156,9 +155,126 @@ void write_entry_to_secure_log(const secure_log the_secure_log,
    return;
 }
 
+// Refinces Cryptol validFirstEntry
+bool valid_first_entry (const secure_log the_secure_log)
+{
+  const aes256_key dummy_key = {0};
+  sha256_digest new_hmac = {0};
+  secure_log_entry root_entry;
 
+  // 1. Fetch the root block from the file
+  root_entry = Log_IO_Read_Entry (the_secure_log, 0);  
+
+  // 2. Form "hmac key log.msg"
+  hmac (dummy_key, root_entry.the_entry, LOG_ENTRY_LENGTH, &new_hmac[0]);
+
+  // 3. new_hmac and root_entry.the_digest should match
+  for (int i = 0; i < SHA256_DIGEST_LENGTH_BYTES; i++)
+    {
+      if (root_entry.the_digest[i] != new_hmac[i])
+	{
+	  return false;
+	}
+    }
+  return true;
+
+}
+
+// Refinces Cryptol validLogEntry
+bool valid_log_entry (const secure_log_entry this_entry,
+		      const sha256_digest    prev_hash)
+{
+  uint8_t msg[SECURE_LOG_ENTRY_LENGTH];
+  size_t index = 0;
+  sha256_digest new_hash = {0};
+
+  // Concatenate this_entry and prev_hash into msg
+  for (size_t i = 0; i < LOG_ENTRY_LENGTH; i++)
+    {
+      msg[index] = this_entry.the_entry[i];
+      index++;
+    }
+  
+  for (size_t i = 0; i < SHA256_DIGEST_LENGTH_BYTES; i++)
+    {
+      msg[index] = prev_hash[i];
+      index++;
+    }
+  
+  sha256 (msg, SECURE_LOG_ENTRY_LENGTH, &new_hash[0]);
+  
+  // 3. new_hash and this_entry.the_digest should match
+  for (int i = 0; i < SHA256_DIGEST_LENGTH_BYTES; i++)
+    {
+      if (this_entry.the_digest[i] != new_hash[i])
+	{
+	  return false;
+	}
+    }
+  return true;
+}
+
+
+// Refines Cryptol validLogFile
 bool verify_secure_log_security(const secure_log the_secure_log)
 {
+  // A log file is valid if the first (root) entry is valid AND
+  // all the subsequent entries are valid.
+  secure_log_entry root_entry;
+  sha256_digest    prev_hash;
+  secure_log_entry this_entry;
 
-  return false;
+  if (valid_first_entry (the_secure_log))
+    {
+      size_t num_entries;
+      num_entries = Log_IO_Num_Entries (the_secure_log);
+      switch (num_entries)
+	{
+	case 0:
+	  // a valid first entry, but apparently zero entries... something
+	  // must be wrong, so
+	  return false;
+	  break;
+	case 1:
+	  // One entry must be the first entry and we already know it's OK, so
+	  return true;
+	  break;
+
+	default:
+	  // Fetch the root entry and keep a copy of it Hash in prev_hash
+	  root_entry = Log_IO_Read_Entry (the_secure_log, 0);  
+
+	  // whole array assignment  prev_hash = root_entry.the_digest;
+	  memcpy (&prev_hash[0], &root_entry.the_digest[0], SHA256_DIGEST_LENGTH_BYTES);
+	  
+	  // Two or more entries
+	  for (size_t i = 2; i <= num_entries; i++)
+	    {
+	      // In the file, entries are numbered starting at 0, so we want the
+	      // (i - 1)'th entry...
+	      this_entry = Log_IO_Read_Entry (the_secure_log, (i - 1));
+	      
+              if (valid_log_entry (this_entry, prev_hash))
+		{
+		  // whole array assignment  prev_hash = this_entry.the_digest;
+		  memcpy (&prev_hash[0],
+			  &this_entry.the_digest[0],
+			  SHA256_DIGEST_LENGTH_BYTES);
+		}
+	      else
+		{
+		  return false;
+		}
+	    }
+	  // Loop must have successfully verified all entries, so
+	  return true;
+	  break;
+	}
+      
+    }
+  else
+    {
+      // First block is invalid, so
+      return false;
+    }
 }
