e1e3f0db055361ba1dd69b1f910436cbc13df623
Daniel M. Zimmerman <dmz@galois.com>
2019-06-14 16:50:36 -0700

    Missing button and paper sensor defines.

--- source/sbb/sbb.h
+++ source/sbb/sbb.h
@@ -39,6 +39,16 @@ extern firmware_state the_firmware_state;
 // these reads/writes.
 extern uint8_t gpio_mem[8];
 
+/* Button defines */
+#define BUTTON_CAST_LED 3
+#define BUTTON_SPOIL_LED 1
+#define BUTTON_CAST_IN 2
+#define BUTTON_SPOIL_IN 0
+
+/* Paper sensor inputs */
+#define PAPER_SENSOR_OUT 6
+#define PAPER_SENSOR_IN 7
+
 // Motor defines
 #define MOTOR_0 4
 #define MOTOR_1 5
