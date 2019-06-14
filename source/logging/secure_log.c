f10f2730674aed6af8d0f6e4593cdf54d366c1b9
Rod Chapman <rod@proteancode.com>
2019-06-14 12:51:35 +0100

    correct typo - previos -> previous

--- source/logging/secure_log.c
+++ source/logging/secure_log.c
@@ -15,7 +15,7 @@ const aes256_key test_key = { 0 };
 
 //keep  sha256_digest
 
-static sha256_digest previos_hash;
+static sha256_digest previous_hash;
 
 // Local functions
 
@@ -63,27 +63,27 @@ void create_secure_log(Log_Handle *secure_log,
 {
   Log_FS_Result create_result, write_result, sync_result;
   secure_log_entry initial_entry;
-  
+
   // Initial/Draft pseudo-code by RCC
 
   // 1. Create new file, open for writing only.
   create_result = Log_IO_Create_New (secure_log, the_secure_log_name);
-  
+
   // 2. call initial_log_entry above to create the first block
   initial_entry = initial_log_entry (test_key, a_log_entry_type);
- 
+
   // 2.1 @dragan keep the first hash
     for (size_t i = 0; i < SHA256_DIGEST_LENGTH_BYTES; i++)
     {
-      previos_hash[i] = initial_entry.the_digest[i];
+      previous_hash[i] = initial_entry.the_digest[i];
     }
-  
+
   // 3. Write that new block to the file.
   write_result = Log_IO_Write_Entry (secure_log, initial_entry);
-  
+
   // 4. sync the file.
   sync_result = Log_IO_Sync (secure_log);
-  
+
   // TBDs - what about error cases?
   //   What if the file already exists? Perhaps a pre-condition here that it doesn't
   //    already exist, so up to the caller to spot that and do the right thing...
@@ -91,7 +91,7 @@ void create_secure_log(Log_Handle *secure_log,
   //    that if not directly supported by ff.h
   //   What if the file create fails?
   //   What is the file write fails?
-  
+
 }
 
 void write_entry_to_secure_log(const secure_log the_secure_log,
@@ -102,13 +102,13 @@ void write_entry_to_secure_log(const secure_log the_secure_log,
   sha256_digest new_hash;
   uint8_t msg[SECURE_LOG_ENTRY_LENGTH]; // appended message
   size_t index=0;
-  
+
 
   // 0. Assume a_log_entry is already padded with zeroes
-  
-  // 1. Introduce a local (static, file-scope) variable previos_hash. Set it above in
+
+  // 1. Introduce a local (static, file-scope) variable previous_hash. Set it above in
   //    create_secure_log for the first time.
-  
+
   // 2. Form the hash value from the message and previous_hash as per the Cryptol spec.
   // hash (paddedMsg # previousHash) is secure_log_entry I guess.
   // adding padded a_log_entry
@@ -116,33 +116,33 @@ void write_entry_to_secure_log(const secure_log the_secure_log,
     {
       msg[index] = a_log_entry[i];
       index++;
-    } 
-  
+    }
+
   // adding previous_hash
    for (size_t i = 0; i < SHA256_DIGEST_LENGTH_BYTES; i++)
     {
-      msg[index] = previos_hash[i];
+      msg[index] = previous_hash[i];
       index++;
-    } 
-   
+    }
+
    // invoke hash ( paddedMsg # previousHash)
    sha256 (msg,SECURE_LOG_ENTRY_LENGTH, &current_entry.the_digest[0]);
-   
+
    // Add the a_log_entry to the current_entry
 
   for (size_t i = 0; i < LOG_ENTRY_LENGTH; i++)
     {
       current_entry.the_entry[i] = a_log_entry[i];
-    } 
+    }
 
   // 3. Save the new hash to previous_hash
   for (size_t i = 0; i < SHA256_DIGEST_LENGTH_BYTES; i++)
     {
-      previos_hash[i] = new_hash[i];
+      previous_hash[i] = new_hash[i];
     }
-   
+
   // 4. Write the log_entry message to the_secure_log
-  
+
   write_result = Log_IO_Write_Entry (the_secure_log, current_entry);
 
   // 5. Write the hash block
