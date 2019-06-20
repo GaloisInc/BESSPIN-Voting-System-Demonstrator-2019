eb2298ffc54cd9ed089c059d364885eff1ad097a
Joseph Kiniry <kiniry@freeandfair.us>
2019-06-19 17:33:05 -0700

    Implemented first scenario test and extended build appropriately.

--- source/logging/log_main.c
+++ source/logging/log_main.c
@@ -12,8 +12,11 @@
 
 void Empty_Log_Smoketest(const log_name the_log_name,
                          const log_io_stream a_target) {
-  assert(false && "unimplemented");
-  //@ assert false;
+  // @todo kiniry This will only work on FreeRTOS.
+  Log_Handle my_log;
+  Log_IO_Initialize();
+  create_log(&my_log, the_log_name);
+  export_log(&my_log, a_target);
   return;
 }
 
@@ -39,12 +42,15 @@ void Import_Export_Non_Empty_Log(const log_name the_log_name,
 }
 
 int main(int argc, char* argv[]) {
-  // @todo kiniry The use of STDERR and printf needs to be refactored
-  // to an appropriate calls on FreeRTOS.
+  char* smoketest_log = "smoketest.log";
+  
+  // @todo kiniry The use of `stderr` and `printf` needs to be
+  // refactored to use appropriate calls on FreeRTOS when building to
+  // that target.
   if (argc == 1)
-    Empty_Log_Smoketest("smoketest.log", stderr);
+    Empty_Log_Smoketest(smoketest_log, stderr);
   else if (argc == 2 && strncmp("smoketest", argv[1], 9) == 0)
-    Empty_Log_Smoketest("smoketest.log", stderr);
+    Empty_Log_Smoketest(smoketest_log, stderr);
   else if (argc == 3 && strncmp("smoketest", argv[1], 9) == 0)
     Empty_Log_Smoketest(argv[2], stderr);
   else
--- source/logging/log_main.h
+++ source/logging/log_main.h
@@ -1,6 +1,7 @@
 // Implementation of all Logging subsystem scenarios.
 
 #include "log.h"
+#include "log_io.h"
 
 void Empty_Log_Smoketest(const log_name the_log_name,
                          const log_io_stream a_target);
@@ -10,3 +11,5 @@ void Non_Empty_Log_Smoketest(const log_name the_log_name,
                              const log_io_stream a_target);
 void Import_Export_Non_Empty_Log(const log_name the_log_name,
                                  const log_io_stream a_target);
+
+const log_entry an_empty_entry = "                                                                                                                                                                                                                                                                ";
