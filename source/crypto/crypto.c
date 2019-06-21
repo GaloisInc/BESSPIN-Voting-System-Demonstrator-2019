89069f352b9555400403b662846d06ef9791091b
Rod Chapman <rod@proteancode.com>
2019-06-21 06:29:01 +0100

    Implment AES-CBC-MAC
    1. Add spec and draft body to crypto module
    2. Add named typedef for an aes128_block
    3. Update crypto_bottom.c accordingly
    4. Fix Makefile.bottom to avoid failure
    
    TBD
    1. Test cases
    2. Remove HMAC

--- source/crypto/crypto.c
+++ source/crypto/crypto.c
@@ -4,6 +4,7 @@
  */
 
 // General includes
+#include <assert.h>
 
 // Subsystem includes
 #include "crypto.h"
@@ -75,4 +76,46 @@ void hmac(message the_message, size_t the_message_size, digest the_digest)
     SHA256Update(&context, the_message, the_message_size);
 
     SHA256Final(the_digest, &context);
+}
+
+void aes_cbc_mac(message the_message, size_t the_message_size, block the_digest)
+{
+    AES_KEY key_schedule;
+    size_t block_count;
+    uint8_t *current_data_ptr = the_message;
+    aes128_block iv = {0};
+    aes128_block plaintext_block = {0};
+    aes128_block ciphertext_block = {0};
+
+    assert(the_message_size % AES_BLOCK_LENGTH_BYTES == 0);
+
+    // Only fails if mock_key == NULL || &key_schedule == NULL
+    AES_set_encrypt_key(mock_key, AES256_KEY_LENGTH_BITS, &key_schedule);
+
+    block_count = the_message_size / AES_BLOCK_LENGTH_BYTES;
+
+    // For each block of input data
+    for (int b = 0; b < block_count; b++)
+    {
+        // Input data to encrypt is IV xor Mi
+        for (size_t i = 0; i < AES_BLOCK_LENGTH_BYTES; i++)
+        {
+            plaintext_block[i] = iv[i] ^ (*current_data_ptr);
+            current_data_ptr++;
+        }
+        AES_encrypt(plaintext_block, ciphertext_block, &key_schedule);
+
+        // Copy the newly encrypted block back into IV for the next time.
+        for (size_t j = 0; j < AES_BLOCK_LENGTH_BYTES; j++)
+        {
+            iv[j] = ciphertext_block[j];
+        }
+    }
+
+    // When the loop terminates, the final MAC value is the result of the final
+    // encryption, which is now in both ciphertext_block and iv
+    for (size_t i = 0; i < AES_BLOCK_LENGTH_BYTES; i++)
+    {
+        the_digest[i] = iv[i];
+    }
 }
