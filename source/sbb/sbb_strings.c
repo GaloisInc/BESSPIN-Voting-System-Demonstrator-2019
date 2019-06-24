0ca0b6812254f8950e13f952423d04099d7dee23
Daniel M. Zimmerman <dmz@galois.com>
2019-06-24 10:06:34 -0700

    Pushed the new `sbb_strings.c` file.

---
+++ source/sbb/sbb_strings.c
@@ -0,0 +1,18 @@
+/**
+ * Smart Ballot Box API
+ * @refine sbb.lando
+ */
+
+#include "sbb.h"
+
+// String constant defines
+const char *welcome_text = "Welcome, Voter!";
+const char *insert_ballot_text = "Insert ballot...";
+const char *barcode_detected_text = "Barcode detected";
+const char *cast_or_spoil_line_1_text = " Cast or Spoil?";
+const char *cast_or_spoil_line_2_text = "Spoil       Cast"; // spoil button on left
+const char *casting_ballot_text = "Casting ballot.";
+const char *spoiling_ballot_text = "Spoiling ballot.";
+const char *invalid_barcode_text = "Invalid barcode.";
+const char *no_barcode_text = "No barcode.";
+const char *remove_ballot_text = "Remove ballot...";
