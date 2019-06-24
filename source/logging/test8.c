fe77b0f545380698a2dac32fb9842e309485cede
dragan <dragan@freeandfair.us>
2019-06-24 18:59:11 +0100

    added labels to non Free RTOS impl

--- source/logging/test8.c
+++ source/logging/test8.c
@@ -19,7 +19,7 @@ uint8_t compare_logs_by_bytes(Log_Handle* first_log, Log_Handle *second_log)
 
     if (lsize_first != lsize_second) {
         printf("%s\n","Failure - sizes are not equal" );
-        return -1;
+        return LOG_FS_ERROR;
     }
 
     for (size_t i=0; i < lsize_first; i++) {
@@ -27,11 +27,11 @@ uint8_t compare_logs_by_bytes(Log_Handle* first_log, Log_Handle *second_log)
         fread(&byte2, 1, 1, &second_log -> the_file);
         if (byte1 != byte2) {
             printf("%s\n","Failure - bytes are not equal" );
-            return -1;
+            return LOG_FS_ERROR;
         }
     }
     printf("%s\n","both logs are equal!");
-    return 0;
+    return LOG_FS_OK;
 }
 
 int main(void)
