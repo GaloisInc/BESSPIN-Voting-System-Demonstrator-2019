686cbc84c8fc9d30ab94814242b4b6b9d6fbbbe9
dragan <dragan@freeandfair.us>
2019-06-24 15:40:56 +0100

    test8.c added with minor description

--- source/logging/test8.c
+++ source/logging/test8.c
@@ -7,12 +7,12 @@ uint8_t compare_logs_by_bytes(Log_Handle* first_log, Log_Handle *second_log)
     size_t      lsize_first, lsize_second; // log sizes
     uint8_t      byte1, byte2;
     
-    // get the file size
+    // get the file size of the first log
     fseek (&first_log -> the_file, 0 , SEEK_END);
     lsize_first = ftell (&first_log -> the_file);
     rewind (&first_log -> the_file);
     
-    // get the file size
+    // get the file size of the second log
     fseek (&second_log -> the_file , 0 , SEEK_END);
     lsize_second = ftell (&second_log -> the_file);
     rewind (&second_log -> the_file);
@@ -21,6 +21,7 @@ uint8_t compare_logs_by_bytes(Log_Handle* first_log, Log_Handle *second_log)
         printf("%s\n","Failure - sizes are not equal" );
         return -1;
     }
+
     for (size_t i=0; i < lsize_first; i++) {
         fread(&byte1, 1, 1, &first_log -> the_file);
         fread(&byte2, 1, 1, &second_log -> the_file);
