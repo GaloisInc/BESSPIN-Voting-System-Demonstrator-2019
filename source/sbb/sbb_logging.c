701b2d4e04ba54ce37d484a6a8dd9ec279fc6819
Alexander Bakst <abakst@galois.com>
2019-06-27 16:36:13 -0700

    import logs if they exist

--- source/sbb/sbb_logging.c
+++ source/sbb/sbb_logging.c
@@ -6,9 +6,20 @@
 const log_name system_log_file_name = "system_log.txt";
 const log_name app_log_file_name    = "application_log.txt";
 
+void load_or_create(log_file *the_file, const log_name the_name) {
+  if ( Log_IO_File_Exists(the_name) ) {
+    *the_file = import_log(the_name);
+    verify_log_well_formedness(*the_file);
+    // @todo handle errors when this procedure is updated
+  } else {
+    create_log(the_file, the_name);
+    // @todo handle errors when this procedure is updated
+  }
+}
+
 void load_or_create_logs(void) {
-  create_log(app_log_file, app_log_file_name);
-  create_log(system_log_file, system_log_file_name);
+  load_or_create(app_log_file, app_log_file_name);
+  load_or_create(system_log_file, system_log_file_name);
 }
 
 void log_system_message(const log_entry new_entry) {
