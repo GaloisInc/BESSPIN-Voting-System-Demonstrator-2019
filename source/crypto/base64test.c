8402236bffdacce1544a078c59548f9cc539a4de
dragan <dragan@freeandfair.us>
2019-07-01 14:56:06 +0100

    compare fixed

--- source/crypto/base64test.c
+++ source/crypto/base64test.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-void compare(unsigned char *s1, unsigned char *s2){
-    if (memcmp(s1, s2, sizeof(s1) - 1) == 0) {
+void compare(unsigned char *s1, unsigned char *s2, size_t num ){
+    if (memcmp(s1, s2, num) == 0) {
         printf("%s\n","Success!\n");
     }else {
         printf("%s\n","Failure!\n");
@@ -32,7 +32,7 @@ int main()
 
     decode(encoded_string, decoded_string);
     printf("decoded=%s\n", decoded_string);
-    compare(src_string_padding_1_20, decoded_string);
+    compare(src_string_padding_1_20, decoded_string, 20);
 
     // padding 2 lendth 19
 
@@ -48,7 +48,7 @@ int main()
 
     decode(encoded_string, decoded_string);
     printf("decoded=%s\n", decoded_string);
-    compare(src_string_padding_2_19, decoded_string);
+    compare(src_string_padding_2_19, decoded_string, 19);
 
     // padding 0 lendth 18
 
@@ -64,7 +64,7 @@ int main()
 
     decode(encoded_string, decoded_string);
     printf("decoded=%s\n", decoded_string);
-    compare(src_string_padding_0_18, decoded_string);
+    compare(src_string_padding_0_18, decoded_string, 18);
 
      // padding 1 lendth 17
 
@@ -80,7 +80,7 @@ int main()
 
     decode(encoded_string, decoded_string);
     printf("decoded=%s\n", decoded_string);
-    compare(src_string_padding_1_17, decoded_string);
+    compare(src_string_padding_1_17, decoded_string, 17);
 
       // padding 2 lendth 16
 
@@ -96,7 +96,7 @@ int main()
 
     decode(encoded_string, decoded_string);
     printf("decoded=%s\n", decoded_string);
-    compare(src_string_padding_2_16, decoded_string);
+    compare(src_string_padding_2_16, decoded_string, 16);
      
     printf("%s\n", "test log entry:");
     unsigned char src_entry[]="hello "
@@ -115,12 +115,12 @@ int main()
     printf("decoded=%s\n", decoded_string);
 
     printf("%s\n", "Test invariant decode ( encode )");
-    compare(src_entry, decoded_string);
+    compare(src_entry, decoded_string, 256);
 
     printf("%s\n", "Test invariant encode ( decode )");
     len = *obtain_encode_buffer_size(decoded_string);
     encode(decoded_string, dst, len);
-    compare(encoded_string, dst);
+    compare(encoded_string, dst, 256);
 
     printf("%s\n", "Test that 32 bytes become 44 bytes when Base64 encoded");
     unsigned char src_string_32[]="dependingonwhethertheobjectpoint";
