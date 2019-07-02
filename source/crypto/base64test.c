781c951097c4db80c1406e564991e5a43b68028a
Rod Chapman <rod@proteancode.com>
2019-07-02 12:05:09 +0100

    Correct value of dlen for call to decode to meet ACSL contract/assertion

--- source/crypto/base64test.c
+++ source/crypto/base64test.c
@@ -12,21 +12,21 @@ int main()
     size_t olen;
     size_t olen1;
     int r;
-    
+
     printf("%s\n", "Test that 32 bytes become 44 bytes when Base64 encoded");
 
     r = mbedtls_base64_encode (dst, 0, &olen, src, 32);
     printf("required encoder dlen=%zu\n", olen);
-    
+
 
     r = mbedtls_base64_encode (dst, 46, &olen, src, 32);
     printf("Encoded:%s\n", dst);
     printf("actual encoder olen=%zu\n", olen);
-    
+
     r = mbedtls_base64_decode (NULL,0, &olen1, src_encoded, 44);
     printf("required decoder dlen=%zu\n", olen1);
 
-    r = mbedtls_base64_decode (from_encoded_dst,32, &olen1, src_encoded, 44);
+    r = mbedtls_base64_decode (from_encoded_dst,33, &olen1, src_encoded, 44);
 
     printf("actual decode olen=%zu\n", olen);
     printf("r=%d\n", r);
