2d885508f3d3ebf96dad8499abfc6040e55e70cf
dragan <dragan@freeandfair.us>
2019-07-09 22:53:53 +0100

    message introduced

--- source/logging/log_net.c
+++ source/logging/log_net.c
@@ -20,28 +20,28 @@ void Log_NET_Initialize() {
 
 void Log_NET_Send(base64_secure_log_entry secure_log_entry, log_name file_name) {
 	// create header
-	size_t length = BASE64_SECURE_BLOCK_LOG_ENTRY_LENGTH;
-
+	size_t len = BASE64_SECURE_BLOCK_LOG_ENTRY_LENGTH;
+    size_t FIXED_MESSAGE_SIZE = 134;
     char *log_file_name = file_name;
 
-    size_t HEADER_SIZE = 134 + strlen(log_file_name);
+    size_t MESSAGE_SIZE = FIXED_MESSAGE_SIZE + strlen(log_file_name) + len;
 
-    char header[HEADER_SIZE];
+    char message[MESSAGE_SIZE];
 
-    char* HEADER_1 = "POST /";
-    char* HEADER_2 = log_file_name;
-    char* HEADER_3 = " HTTP/1.1\r\n";
-    char* HEADER_4 = "Host: 10.6.6.253\r\n";
-    char* HEADER_5 = "User-Agent: sbb/2019\r\n";
-    char* HEADER_6 = "Accept: */*\r\n";
-    char* HEADER_7 = "Content-Type: application/octet-stream\r\n";
-    char* HEADER_8 = "Content-Length: ";
-    size_t HEADER_9 = length;
-    char* HEADER_10="\r\n\r\n";
-    snprintf(header, HEADER_SIZE, "%s%s%s%s%s%s%s%s%zu%s",
-             HEADER_1, HEADER_2, HEADER_3, HEADER_4,
-             HEADER_5, HEADER_6, HEADER_7, HEADER_8,
-             HEADER_9, HEADER_10);
+    char* REQUEST_LINE_1 = "POST /";
+    char* REQUEST_LINE_2 = log_file_name;
+    char* REQUEST_LINE_3 = " HTTP/1.1\r\n";
+    char* HEADER_1 = "Host: 10.6.6.253\r\n";
+    char* HEADER_2 = "User-Agent: sbb/2019\r\n";
+    char* HEADER_3 = "Accept: */*\r\n";
+    char* HEADER_4 = "Content-Type: application/octet-stream\r\n";
+    char* HEADER_5_1 = "Content-Length: ";
+    size_t HEADER_5_2 = len;
+    char* DOUBLE_CRLF = "\r\n\r\n";
+    snprintf(message, MESSAGE_SIZE, "%s%s%s%s%s%s%s%s%zu%s",
+             REQUEST_LINE_1, REQUEST_LINE_2, REQUEST_LINE_3, HEADER_1,
+             HEADER_2, HEADER_3, HEADER_4, HEADER_5_1,
+             HEADER_5_2, DOUBLE_CRLF);
 	return;
 }
 
