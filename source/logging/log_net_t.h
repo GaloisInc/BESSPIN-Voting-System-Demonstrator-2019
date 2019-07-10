bf2d9e073325726f4d9340d0074fd52bccd06e83
Rod Chapman <rod@proteancode.com>
2019-07-10 12:41:00 +0100

    Add log_net_t.h

---
+++ source/logging/log_net_t.h
@@ -0,0 +1,11 @@
+#ifndef __LOG_NET_T_H__
+#define __LOG_NET_T_H__
+
+typedef enum
+{
+    HTTP_Endpoint_App_Log = 0,
+    HTTP_Endpoint_Sys_Log,
+    HTTP_Endpoint_None
+} http_endpoint;
+
+#endif /* __LOG_IO_T_H__ */
