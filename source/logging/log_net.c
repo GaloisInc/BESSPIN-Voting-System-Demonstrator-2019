ee507523f73ec22bfeb1fad5db06123792ee25cf
Rod Chapman <rod@proteancode.com>
2019-07-11 10:47:55 +0100

    Resolve compilation issues for FreeRTOS. Remove compiler warning, and refactor to minimize visibility of local constants.

--- source/logging/log_net.c
+++ source/logging/log_net.c
@@ -16,6 +16,7 @@ static const char *HEADER_5_1 = "Content-Length: ";
 static const char *DOUBLE_CRLF = "\r\n\r\n";
 
 static const size_t data_block_length = BASE64_SECURE_BLOCK_LOG_ENTRY_LENGTH;
+static const uint16_t PORT_NUMBER = 8066;
 
 // We assume that a log entry data block can't be more than 9_999_999 bytes long, so
 // we allocate up to 7 characters for this to be printed in decimal in the HTTP header.
@@ -23,22 +24,47 @@ static const size_t worst_case_data_length = 7;
 
 #ifdef TARGET_OS_FreeRTOS
 
+/////////////////////////////
+// FreeRTOS implementation //
+/////////////////////////////
+
+/* FreeRTOS and IP stack includes. */
+#include "FreeRTOS.h"
+#include "FreeRTOS_IP.h"
+#include "FreeRTOS_Sockets.h"
+#include "list.h"
+
+// Network configuration and addresses that are the same for all SBBs
+static const uint8_t SBB_NetMask[4] = {255, 255, 255, 0};
+static const uint8_t SBB_GatewayAddress[4] = {10, 10, 10, 1};
+static const uint8_t SBB_DNSServerAddress[4] = {4, 2, 2, 2};
+static const uint8_t Reporting_Server_IP_Address[4] = {10, 6, 6, 253};
+
+// Network configuration and addresses that will be different for each SBB
+
+// RCC made this up - to be confirmed
+// Should be different for each SBB
+static uint8_t This_SBB_IP_Address[4] = {10, 6, 6, 1};
+
+// RCC No idea where these numbers came from - to be confirmed
+// Should be different for each SBB
+static uint8_t This_SBB_MAC_Address[6] = {0x00, 0x12, 0x13, 0x10, 0x15, 0x11};
+
 void Log_Net_Initialize()
 {
     debug_printf("FreeRTOS_IPInit\r\n");
-    FreeRTOS_IPInit(uIPAddress, uNetMask, uGatewayAddress, uDNSServerAddress,
-                    uMACAddress);
+    FreeRTOS_IPInit(This_SBB_IP_Address, SBB_NetMask, SBB_GatewayAddress,
+                    SBB_DNSServerAddress, This_SBB_MAC_Address);
 }
 
-void Log_Net_Send(base64_secure_log_entry secure_log_entry,
+void Log_Net_Send(base64_secure_log_entry the_secure_log_entry,
                   http_endpoint endpoint, const char *remote_file_name)
 {
     Socket_t xSocket;
     struct freertos_sockaddr xRemoteAddress;
-    BaseType_t xAlreadyTransmitted = 0, xBytesSent = 0;
-    TaskHandle_t xRxTask = NULL; // it is defined from some reason ?
+    BaseType_t xBytesSent = 0;
+
     size_t xLenToSend;
-    size_t PORT_NUMBER = 8066;
     size_t xTotalLengthToSend;
 
     // request line and header
@@ -67,9 +93,10 @@ void Log_Net_Send(base64_secure_log_entry secure_log_entry,
         pcBufferToTransmit[i] = 0x00;
     }
 
-    vsnprintf(pcBufferToTransmit, MESSAGE_SIZE, "%s%s%s%s%s%s%s%s%zu%s", REQUEST_LINE_1,
-             remote_file_name, REQUEST_LINE_3, HEADER_1, HEADER_2, HEADER_3,
-             HEADER_4, HEADER_5_1, data_block_length, DOUBLE_CRLF);
+    snprintf(pcBufferToTransmit, MESSAGE_SIZE, "%s%s%s%s%s%s%s%s%zu%s",
+             REQUEST_LINE_1, remote_file_name, REQUEST_LINE_3, HEADER_1,
+             HEADER_2, HEADER_3, HEADER_4, HEADER_5_1, data_block_length,
+             DOUBLE_CRLF);
 
     // After the header has been written, we have N bytes of header,
     // occupying bytes 0 .. (N-1) of pcBufferToTransmit. So.. the first byte of the
@@ -79,8 +106,8 @@ void Log_Net_Send(base64_secure_log_entry secure_log_entry,
     debug_printf("Length of header block is %zu\n", first_byte_of_data_index);
 
     // add base64_secure_log_entry to the pcBufferToTransmit
-    memcpy(&pcBufferToTransmit[first_byte_of_data_index], &secure_log_entry.the_entry[0],
-           LOG_ENTRY_LENGTH);
+    memcpy(&pcBufferToTransmit[first_byte_of_data_index],
+           &the_secure_log_entry.the_entry[0], LOG_ENTRY_LENGTH);
 
     size_t space_index = first_byte_of_data_index + LOG_ENTRY_LENGTH;
     debug_printf("space index is %zu\n", space_index);
@@ -88,7 +115,7 @@ void Log_Net_Send(base64_secure_log_entry secure_log_entry,
 
     size_t hash_index = space_index + 1;
     debug_printf("hash index is %zu\n", hash_index);
-    memcpy(&pcBufferToTransmit[hash_index], &secure_log_entry.the_digest[0],
+    memcpy(&pcBufferToTransmit[hash_index], &the_secure_log_entry.the_digest[0],
            SHA256_BASE_64_DIGEST_LENGTH_BYTES);
 
     size_t new_line_index = hash_index + SHA256_BASE_64_DIGEST_LENGTH_BYTES;
@@ -98,54 +125,69 @@ void Log_Net_Send(base64_secure_log_entry secure_log_entry,
     // of bytes to send is N + 1
     xTotalLengthToSend = new_line_index + 1;
 
-    xRemoteAddress.sin_port = FreeRTOS_htons( PORT_NUMBER );
+    xRemoteAddress.sin_port = FreeRTOS_htons(PORT_NUMBER);
     // IP address needs to be modified for the test purpose
     // otherwise address can be taken from log_net.h uIPAddress
     // for now it is hardcoded before test.
-    xRemoteAddress.sin_addr = FreeRTOS_inet_addr_quick( 10, 6, 6 253); 
+    xRemoteAddress.sin_addr = FreeRTOS_inet_addr_quick(
+        Reporting_Server_IP_Address[0], Reporting_Server_IP_Address[1],
+        Reporting_Server_IP_Address[2], Reporting_Server_IP_Address[3]);
 
     // Create a socket.
-    xSocket = FreeRTOS_socket( FREERTOS_AF_INET,
-                               FREERTOS_SOCK_STREAM,
-                               FREERTOS_IPPROTO_TCP );
-    configASSERT( xSocket != FREERTOS_INVALID_SOCKET );
-    if( FreeRTOS_connect( xSocket, &xRemoteAddress, sizeof( xRemoteAddress ) ) == 0 )
+    xSocket = FreeRTOS_socket(FREERTOS_AF_INET, FREERTOS_SOCK_STREAM,
+                              FREERTOS_IPPROTO_TCP);
+    configASSERT(xSocket != FREERTOS_INVALID_SOCKET);
+    if (FreeRTOS_connect(xSocket, &xRemoteAddress, sizeof(xRemoteAddress)) == 0)
     {
-        xLenToSend=0;
+        xLenToSend = 0;
         do
-        { 
-            xBytesSent = FreeRTOS_send( /* The socket being sent to. */
-                                        xSocket,
-                                        /* The data being sent. */
-                                        pcBufferToTransmit + xLenToSend,
-                                        /* The remaining length of data to send. */
-                                        xTotalLengthToSend - xLenToSend,
-                                        /* ulFlags. */
-                                        0 );
-            if( xBytesSent < 0)
+        {
+            xBytesSent =
+                FreeRTOS_send(/* The socket being sent to. */
+                              xSocket,
+                              /* The data being sent. */
+                              pcBufferToTransmit + xLenToSend,
+                              /* The remaining length of data to send. */
+                              xTotalLengthToSend - xLenToSend,
+                              /* ulFlags. */
+                              0);
+            if (xBytesSent < 0)
             {
                 debug_printf("ERROR writing message to socket");
             }
 
-            if( xBytesSent == 0 )
+            if (xBytesSent == 0)
             {
                 break;
             }
             xLenToSend += xBytesSent;
-        } while ( xLenToSend < xTotalLengthToSend );
+        } while (xLenToSend < xTotalLengthToSend);
     }
     /* Initiate graceful shutdown. */
-    FreeRTOS_shutdown( xSocket, FREERTOS_SHUT_RDWR );
+    FreeRTOS_shutdown(xSocket, FREERTOS_SHUT_RDWR);
     /* The socket has shut down and is safe to close. */
-    FreeRTOS_closesocket( xSocket );
+    FreeRTOS_closesocket(xSocket);
     return;
 }
 
 #else
 
+//////////////////////////////////////////////
+// POSIX implementation for MacOS and Linux //
+//////////////////////////////////////////////
+
+// @assume We have a POSIX I/O filesystem.
+#include <netdb.h>      /* struct hostent, gethostbyname */
+#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
+#include <stdio.h>      /* printf, sprintf */
+#include <stdlib.h>     /* exit */
+#include <string.h>     /* memcpy, memset */
+#include <sys/socket.h> /* socket, connect */
+#include <unistd.h>     /* read, write, close */
+
 void Log_Net_Initialize() { return; }
 
-void Log_Net_Send(base64_secure_log_entry secure_log_entry,
+void Log_Net_Send(base64_secure_log_entry the_secure_log_entry,
                   http_endpoint endpoint, const char *remote_file_name)
 {
     const size_t FIXED_MESSAGE_SIZE =
@@ -163,7 +205,6 @@ void Log_Net_Send(base64_secure_log_entry secure_log_entry,
     char *host = "localhost";
     struct hostent *server;
     struct sockaddr_in serv_addr;
-    int PORT_NUMBER = 8066;
 
     debug_printf("MESSAGE_SIZE is %zu\n", MESSAGE_SIZE);
 
@@ -193,8 +234,8 @@ void Log_Net_Send(base64_secure_log_entry secure_log_entry,
     debug_printf("Length of header block is %zu\n", first_byte_of_data_index);
 
     // add base64_secure_log_entry to the message
-    memcpy(&message[first_byte_of_data_index], &secure_log_entry.the_entry[0],
-           LOG_ENTRY_LENGTH);
+    memcpy(&message[first_byte_of_data_index],
+           &the_secure_log_entry.the_entry[0], LOG_ENTRY_LENGTH);
 
     size_t space_index = first_byte_of_data_index + LOG_ENTRY_LENGTH;
     debug_printf("space index is %zu\n", space_index);
@@ -202,7 +243,7 @@ void Log_Net_Send(base64_secure_log_entry secure_log_entry,
 
     size_t hash_index = space_index + 1;
     debug_printf("hash index is %zu\n", hash_index);
-    memcpy(&message[hash_index], &secure_log_entry.the_digest[0],
+    memcpy(&message[hash_index], &the_secure_log_entry.the_digest[0],
            SHA256_BASE_64_DIGEST_LENGTH_BYTES);
 
     size_t new_line_index = hash_index + SHA256_BASE_64_DIGEST_LENGTH_BYTES;
--- source/logging/log_net.h
+++ source/logging/log_net.h
@@ -1,77 +1,19 @@
 #ifndef __LOG_NET_H__
 #define __LOG_NET_H__
 
-#include "secure_log_t.h"
 #include "log_net_t.h"
+#include "secure_log_t.h"
 
-#ifdef TARGET_OS_FreeRTOS
+// Abstract ghost state representing the overall state
+// of the reporting system
 
-/* FreeRTOS  includes. */
-#include "FreeRTOS.h"
+//@ ghost int reporting_system;
 
-/* IP stack includes. */
-#include "FreeRTOS_IP.h"
-#include "FreeRTOS_Sockets.h"
-
-// dragan values needs to be changed
-
-// ucIPAddress
-#define configIP_ADDR0			10
-#define configIP_ADDR1			6
-#define configIP_ADDR2			6
-#define configIP_ADDR3			253
-
-// ucNetMask
-#define configNET_MASK0			255
-#define configNET_MASK1			255
-#define configNET_MASK2			255
-#define configNET_MASK3			0
-
-// ucGatewayAddress
-#define configGATEWAY_ADDR0		10
-#define configGATEWAY_ADDR1		10
-#define configGATEWAY_ADDR2		10
-#define configGATEWAY_ADDR3		1
-
-// ucDNSServerAddress
-#define configDNS_SERVER_ADDR0	4
-#define configDNS_SERVER_ADDR1	2
-#define configDNS_SERVER_ADDR2	2
-#define configDNS_SERVER_ADDR3	2
-
-// ucMACAddress
-#define configMAC_ADDR0			0x00
-#define configMAC_ADDR1			0x12
-#define configMAC_ADDR2			0x13
-#define configMAC_ADDR3			0x10
-#define configMAC_ADDR4			0x15
-#define configMAC_ADDR5			0x11
-
-uint8_t uIPAddress[4] = {configIP_ADDR0, configIP_ADDR1, configIP_ADDR2, configIP_ADDR3};
-uint8_t uNetMask[4] = {configNET_MASK0, configNET_MASK1, configNET_MASK2, configNET_MASK3};
-uint8_t uGatewayAddress[4] = {configGATEWAY_ADDR0, configGATEWAY_ADDR1, configGATEWAY_ADDR2, configGATEWAY_ADDR3};
-uint8_t uDNSServerAddress[4] = {configDNS_SERVER_ADDR0, configDNS_SERVER_ADDR1, configDNS_SERVER_ADDR2, configDNS_SERVER_ADDR3};
-uint8_t uMACAddress[6] = {configMAC_ADDR0, configMAC_ADDR1, configMAC_ADDR2, configMAC_ADDR3, configMAC_ADDR4, configMAC_ADDR5};
-
-
-#else
-// @assume We have a POSIX I/O filesystem.
-#include <stdio.h> /* printf, sprintf */
-#include <stdlib.h> /* exit */
-#include <unistd.h> /* read, write, close */
-#include <string.h> /* memcpy, memset */
-#include <sys/socket.h> /* socket, connect */
-#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
-#include <netdb.h> /* struct hostent, gethostbyname */
-
-#endif
-
-
+// ACSL Contracts TBD
 void Log_Net_Initialize(void);
 
-void Log_Net_Send(base64_secure_log_entry secure_log_entry,
-                  http_endpoint endpoint,
-                  const char *remote_file_name);
+// ACSL Contracts TBD
+void Log_Net_Send(base64_secure_log_entry the_secure_log_entry,
+                  http_endpoint endpoint, const char *remote_file_name);
 
-
-#endif /* __LOG_IO_H__ */
+#endif /* __LOG_NET_H__ */
