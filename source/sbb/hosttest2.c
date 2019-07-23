e327ec922dcfc210aeccc3ae31982a24b4dd419b
Alexander Bakst <abakst@galois.com>
2019-07-23 11:44:55 -0700

    Update timestamp format; update auth key.

--- source/sbb/hosttest2.c
+++ source/sbb/hosttest2.c
@@ -5,7 +5,7 @@
 
 
 unsigned char *a_barcode =
-    "2019:07:16:18:13:AX99C4QSlcCUg5SIW1sfrApDW9K-Hcq2c1mgBBq3Vf0=";
+    "2019+06+23+17+45:Dgep2UpFCE2XLgx6QZUfW3hlVhINicNbwL9KI8UtUGM=";
 
 int main(void) {
     assert(!crypto_check_barcode_valid(a_barcode, strlen(a_barcode)));
