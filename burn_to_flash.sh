38ff36d57dc9b77c5ad896150d14ada53f342c5d
Michal Podhradsky <mpodhradsky@galois.com>
2019-07-25 18:51:17 -0700

    Script updates

--- burn_to_flash.sh
+++ burn_to_flash.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
+CURRENT_PATH='pwd'
 GFE_REPO_PATH=../gfe
 # make the ballot box binary
 make clean; make;
-# copy the binary to the GFE
-cp main_ballot_box.elf $GFE_REPO_PATH/bootmem/freertos.elf
-#Making the FreeRTOS binary image <bootmem.bin>
-cd $GFE_REPO_PATH/bootmem
-make -f Makefile.freertos clean
-make -f Makefile.freertos
-cd ..
+
+cd $GFE_REPO_PATH
 # run the simple upload flash script
-./upload_flash_simple.sh chisel_p1
+./upload_flash_simple.sh chisel_p1  $CURRENT_PATH/main_ballot_box.elf
