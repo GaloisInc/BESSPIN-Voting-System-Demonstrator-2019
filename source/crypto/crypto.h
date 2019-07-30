568b942dbcec371b2b9ebc97a61d4e0dbb95caae
Rod Chapman <rod@proteancode.com>
2019-07-30 15:18:45 +0100

    Restore assigns contracts here now that other contracts are correct.

--- source/crypto/crypto.h
+++ source/crypto/crypto.h
@@ -21,7 +21,7 @@ extern const aes256_key log_entry_mac_key;
 
 /*@ requires \valid_read(the_plaintext + (0 .. AES_BLOCK_LENGTH_BYTES - 1));
   @ requires \separated(the_plaintext, the_ciphertext);
-  @ // assigns the_ciphertext[0 .. AES_BLOCK_LENGTH_BYTES - 1];
+  @ assigns the_ciphertext[0 .. AES_BLOCK_LENGTH_BYTES - 1];
   @ ensures \initialized(the_ciphertext);
   @ ensures \valid(the_ciphertext + (0 .. AES_BLOCK_LENGTH_BYTES - 1));
   @*/
@@ -29,7 +29,7 @@ void aes_encrypt(plaintext_block the_plaintext, ciphertext_block the_ciphertext,
 
 /*@ requires \valid_read(the_ciphertext + (0 .. AES_BLOCK_LENGTH_BYTES - 1));
   @ requires \separated(the_plaintext, the_ciphertext);
-  @ // assigns the_plaintext[0 .. AES_BLOCK_LENGTH_BYTES - 1];
+  @ assigns the_plaintext[0 .. AES_BLOCK_LENGTH_BYTES - 1];
   @ ensures \initialized(the_plaintext);
   @ ensures \valid(the_plaintext + (0 .. AES_BLOCK_LENGTH_BYTES - 1));
   @*/
@@ -37,7 +37,7 @@ void aes_decrypt(ciphertext_block the_ciphertext, plaintext_block the_plaintext,
 
 /*@ requires \valid_read(the_message + (0 .. the_message_size - 1));
   @ requires \separated(the_message, the_digest);
-  @ // assigns the_digest[0 .. SHA256_DIGEST_LENGTH_BYTES - 1];
+  @ assigns the_digest[0 .. SHA256_DIGEST_LENGTH_BYTES - 1];
   @ ensures \initialized(the_digest);
   @ ensures \valid(the_digest + (0 .. SHA256_DIGEST_LENGTH_BYTES - 1));
   @*/
@@ -46,7 +46,7 @@ void hash(message the_message, size_t the_message_size, digest the_digest);
 /*@ requires \valid_read(the_message + (0 .. the_message_size - 1));
   @ requires \separated(the_message, the_digest);
   @ requires the_message_size % AES_BLOCK_LENGTH_BYTES == 0;
-  @ // assigns the_digest[0 .. AES_BLOCK_LENGTH_BYTES - 1];
+  @ assigns the_digest[0 .. AES_BLOCK_LENGTH_BYTES - 1];
   @ ensures \initialized(the_digest);
   @ ensures \valid(the_digest + (0 .. AES_BLOCK_LENGTH_BYTES - 1));
   @*/
