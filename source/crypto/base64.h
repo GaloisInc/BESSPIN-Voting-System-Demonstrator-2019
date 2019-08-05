e127e009d7760f97a2bca139c303c904c47cd7a0
Dragan Stosic <dragan.stosic@gmail.com>
2019-08-05 19:57:31 +0200

    included base64.acsl

--- source/crypto/base64.h
+++ source/crypto/base64.h
@@ -47,6 +47,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include "base64.acsl"
 
 #define MBEDTLS_ERR_BASE64_BUFFER_TOO_SMALL               -0x002A  /**< Output buffer too small. */
 #define MBEDTLS_ERR_BASE64_INVALID_CHARACTER              -0x002C  /**< Invalid character in input. */
