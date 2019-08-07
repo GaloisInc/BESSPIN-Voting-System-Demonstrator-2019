c32bdb40e6ac41572b751e6c8bc41a6c0d6a0355
Annie Cherkaev <anniecherk@galois.com>
2019-08-07 13:59:09 -0700

    AES example, added missing assembly

---
+++ Attacker-Quickstart/AES-key-to-display/payload.asm
@@ -0,0 +1,7 @@
+		lui     a0, 786568
+        addi    a0, a0, 600
+        sw      a0, -12(s0)
+        lw      a0, -12(s0)
+        lui     a1, 786562
+        addi    a1, a1, 376
+        sw      a1, 0(a0)
