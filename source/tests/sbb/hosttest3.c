16eb681e7aac3b054925a2128d17fede423ab6ec
Alexander Bakst <abakst@galois.com>
2019-09-04 10:41:58 -0700

    Sync tests up with implementation.
    Former-commit-id: 3181eebe78273753a95c83715c17578875e6da47

--- source/tests/sbb/hosttest3.c
+++ source/tests/sbb/hosttest3.c
@@ -8,8 +8,8 @@ char *a_barcode       = "Not a good barcode at all, really shabby stuff";
 char *another_barcode = "Also bad.";
 
 int main(void) {
-    assert(!crypto_check_barcode_valid(a_barcode, strlen(a_barcode)));
-    assert(!crypto_check_barcode_valid(another_barcode, strlen(another_barcode)));
+    assert(BARCODE_VALID != crypto_check_barcode_valid(a_barcode, strlen(a_barcode)));
+    assert(BARCODE_VALID != crypto_check_barcode_valid(another_barcode, strlen(another_barcode)));
 
     return 0;
 }
