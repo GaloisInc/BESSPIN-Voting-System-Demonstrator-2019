569d8b0c869316dc99b235241f782abf2377f68f
Michal Podhradsky <mpodhradsky@galois.com>
2019-10-25 11:54:36 -0700

    Make room for multiple filesystems
    Former-commit-id: 5444b3cd1643daacf415ce43062f43479ff41d37

--- source/include/os/freertos/log_os_defs.h
+++ source/include/os/freertos/log_os_defs.h
@@ -3,9 +3,16 @@
 
 #include <stdint.h>
 
-#include "SDLib.h"
 
+#if TARGET_FS_FAT
+#include "SDLib.h"
 typedef size_t    file_offset;
 typedef char*     osd_file_stream;
-
+#elif TARGET_FS_BLACK_SESAME
+#error "TARGET_FS_LittleFS not yet implemented"
+#else
+// Provide dummy types
+typedef size_t    file_offset;
+typedef char*     osd_file_stream;
+#endif
 #endif // __LOG_FS_FREERTOS_DEFS__H__
